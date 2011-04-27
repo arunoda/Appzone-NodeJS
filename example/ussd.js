9d68c31ed3deac926d12856dfebda49f6b40101c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-04-27 19:34:46 +0530

    Ussd Api's method name changed Remove simplyfication of the header params, which is automatically done by connect

--- example/ussd.js
+++ example/ussd.js
@@ -3,9 +3,9 @@ var appzone = require('../lib/appzone');
 var receiver = appzone.receiver(4444);
 var sender = appzone.sender('http://localhost:8000', "app", "pass");
 
-receiver.onUssdMessage(function(ussd) {
+receiver.onUssd(function(ussd) {
 	console.log("USSD message received!");
-	sender.sendUssdMessage(ussd.address, ussd.conversationId, '---- ' + ussd.message + ' ----', false, function(status) {
+	sender.sendUssd(ussd.address, ussd.conversationId, '---- ' + ussd.message + ' ----', false, function(status) {
 		console.log("USSD MT sent with status: " + JSON.stringify(status));
 	});
 });
