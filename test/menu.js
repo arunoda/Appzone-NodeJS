6391aceeacb435d73eb1b1d4cf12a269b82e2bcc
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-05-30 23:39:16 +0530

    menu object added and tested

---
+++ test/menu.js
@@ -0,0 +1,112 @@
+var menu = require('menu');
+var assert = require('assert');
+
+exports.testsStringItem = function() {
+	
+	var main = menu('main').
+		title('the title').
+		item('one').on('hello');
+	
+	var message = "the title\n1. one\n";
+	assert.equal(main.render(), message);
+	assert.equal(main.when('one'), 'hello');
+	assert.throws(function() {
+		main.when('sdsd');
+	});
+};
+
+exports.testsOtherMenu = function() {
+	
+	var sub = menu('sub').item('one').on('thanks');
+	var main = menu('main').
+		title('the title').
+		item('one').on(sub);
+	
+	var message = "the title\n1. one\n";
+	assert.equal(main.render(), message);
+	assert.equal(main.when('one'), sub);
+};
+
+exports.testsCallbackWithString = function() {
+	
+	var main = menu('main').
+		title('the title').
+		item('one').on(function(itemName) {
+			return itemName + '+++';
+		});
+	
+	var message = "the title\n1. one\n";
+	assert.equal(main.render(), message);
+	assert.equal(main.when('one'), 'one+++');
+};
+
+exports.testsCallbackWithMenu= function() {
+	
+	var sub = menu('sub').item('one').on('thanks');
+	var main = menu('main').
+		title('the title').
+		item('one').on(function(itemName) {
+			return sub;
+		});
+	
+	var message = "the title\n1. one\n";
+	assert.equal(main.render(), message);
+	assert.equal(main.when('one'), sub);
+};
+
+exports.testsErrorWhenOnMethod= function() {
+	
+	var main = menu('main').
+		title('the title');
+	
+	assert.throws(function() {
+		main.item('sdsd').on({});
+	});
+
+};
+
+exports.testsErrorWhenOnMethodIntAdded= function() {
+	
+	var main = menu('main').
+		title('the title');
+	
+	assert.throws(function() {
+		main.item('sdsd').on(1000);
+	});
+
+};
+
+exports.testsNoSuchItem = function() {
+	var main = menu('main').
+	title('the title').
+	setMessageLenghtLimit(100);
+
+	assert.throws(function() {
+		main.when('sdsd');
+	});
+};
+
+exports.testsMessgaeLengthLimitExceeded = function() {
+	
+	assert.throws(function() {
+		var main = menu('main').
+		title('dsfdsfefer').
+		item('dfdds343344444444444444444444444444').on('a').
+		item('dfddsdsssssssssssssssssssssssssssss').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a').
+		item('dfdds').on('a');
+	});
+	
+};
